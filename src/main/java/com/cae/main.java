bfe85475878f3d42aa904fcd1149154018e07d5a
Bryce199805 <bryce199805@gmail.com>
2024-10-26 09:23:23 +0800

    添加json测试样例

--- src/main/java/com/cae/main.java
+++ src/main/java/com/cae/main.java
@@ -8,44 +8,54 @@ public class main {
         CAE db = new CAE(filePath);
 
         // 测试删除
-        if(db.Delete("DELETE FROM basic_ship_information_DB.ship_data_info WHERE ship_data_id = 7082003")){
-            System.out.println("DELETE SUCCESS!");
+//        if(db.Delete("DELETE FROM basic_ship_information_DB.ship_data_info WHERE ship_data_id = 7082003")){
+//            System.out.println("DELETE SUCCESS!");
+//        };
+
+        ResultSetWrapper rsWrapper = new ResultSetWrapper();
+        if(db.Query("select * from SHIP_EQUIPMENT_INFO_DB.EQUI_CLASSIFY_PARADEF where JSON_VALUE(SPECIAL_ATTRIBUTE, '$.AnchorType') = '类型';", rsWrapper)){
+            System.out.println("QUERY SUCCESS!");
+            db.Display(rsWrapper);
+        };
+
+        if(db.Query("select * from SHIP_EQUIPMENT_INFO_DB.EQUI_CLASSIFY_PARADEF where JSON_VALUE(SPECIAL_ATTRIBUTE, '$.\"测试中文\"') = '测试值';", rsWrapper)){
+            System.out.println("QUERY SUCCESS!");
+            db.Display(rsWrapper);
         };
 
         // 测试插入
-        if(db.Insert("INSERT INTO basic_ship_information_DB.ship_data_info (\n" +
-                "    SHIP_DATA_ID, \n" +
-                "    SHIP_NAME, \n" +
-                "    SHIP_TYPE, \n" +
-                "    BUILT, \n" +
-                "    SPEED_SERVICE, \n" +
-                "    OWNER, \n" +
-                "    CLASSIFICATION_DATA, \n" +
-                "    CLASSIFICATION_SOCIETY\n" +
-                ") VALUES (\n" +
-                "    7082001, \n" +
-                "    '航海3号', \n" +
-                "    '油船3', \n" +
-                "    TO_DATE('2020-10-03', 'YYYY-MM-DD'), \n" +
-                "    24.3, \n" +
-                "    '航运有限公司', \n" +
-                "    TO_DATE('2020-11-03', 'YYYY-MM-DD'), \n" +
-                "    '中国船级社CSS'\n" +
-                ");")){
-            System.out.println("INSERT SUCCESS!");
-        };
+//        if(db.Insert("INSERT INTO basic_ship_information_DB.ship_data_info (\n" +
+//                "    SHIP_DATA_ID, \n" +
+//                "    SHIP_NAME, \n" +
+//                "    SHIP_TYPE, \n" +
+//                "    BUILT, \n" +
+//                "    SPEED_SERVICE, \n" +
+//                "    OWNER, \n" +
+//                "    CLASSIFICATION_DATA, \n" +
+//                "    CLASSIFICATION_SOCIETY\n" +
+//                ") VALUES (\n" +
+//                "    7082001, \n" +
+//                "    '航海3号', \n" +
+//                "    '油船3', \n" +
+//                "    TO_DATE('2020-10-03', 'YYYY-MM-DD'), \n" +
+//                "    24.3, \n" +
+//                "    '航运有限公司', \n" +
+//                "    TO_DATE('2020-11-03', 'YYYY-MM-DD'), \n" +
+//                "    '中国船级社CSS'\n" +
+//                ");")){
+//            System.out.println("INSERT SUCCESS!");
+//        };
 
         //测试查询
-        ResultSetWrapper rsWrapper = new ResultSetWrapper();
         if (db.Query("SELECT * FROM basic_ship_information_DB.ship_data_info;",rsWrapper)) {
             db.Display(rsWrapper);
             db.setClose(rsWrapper);
         };
 
         // 测试更新
-        if(db.Update("UPDATE basic_ship_information_DB.ship_data_info SET speed_service = 24.0 WHERE ship_data_id = 7082001;")){
-            System.out.println("UPDATE SUCCESS!");
-        };
+//        if(db.Update("UPDATE basic_ship_information_DB.ship_data_info SET speed_service = 24.0 WHERE ship_data_id = 7082001;")){
+//            System.out.println("UPDATE SUCCESS!");
+//        };
 
 //        db.connectTest(filePath);
         db.connClose();
